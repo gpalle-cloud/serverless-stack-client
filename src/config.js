1935a649a5779efafbbac4b6347afb61ce7e8b81
Geetha Palle <geetha.palle@alchemysystems.com>
2020-05-21 19:55:04 -0500

    setting the react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-geetha"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://u0oc4zccrl.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_XmOWBC8qZ",
+    APP_CLIENT_ID: "27571pjisnmkp9iaj9uqf1a4oi",
+    IDENTITY_POOL_ID: "us-east-1:5475dfa1-b0c8-4adb-bae7-db219b108209"
+  }
+};
